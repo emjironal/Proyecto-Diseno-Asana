11cb82e0ff5d7999c6c1ba33aa3fbd6c02713232
emjironal <emjironal@gmail.com>
2019-05-23 19:23:56 -0600

    Separación en diferentes funciones de parseo

--- control/fabrica/FabricaImportacionProyecto.cs
+++ control/fabrica/FabricaImportacionProyecto.cs
@@ -22,28 +22,76 @@ namespace Proyecto_Diseno_Asana.control.fabrica
             foreach (JObject jObject in data)
             {
                 Tarea tarea = new Tarea();
-                //Obtiene el id de la tarea
-                tarea.codigo = (string)getObjectgFromJObject(jObject, "gid");
-                //Obtiene al encargado
-                JObject assignee = (JObject)getObjectgFromJObject(jObject, "assignee");
-                tarea.encargado = new Usuario();
-                tarea.encargado.id = (string)getObjectgFromJObject(assignee, "gid");
-                tarea.encargado.nombre = (string)getObjectgFromJObject(assignee, "name");
-                //Obtiene la fecha de finalización
-                tarea.isFinalizada = (bool)getObjectgFromJObject(jObject, "completed");
-                if (tarea.isFinalizada)
-                {
-                    tarea.fchFinalizacion = (DateTime)getObjectgFromJObject(jObject, "completed_at");
-                }
-                //Obtiene la fecha de entrega
-                var fchEntrega = getObjectgFromJObject(jObject, "due_on");
-                if ( fchEntrega != null)
-                {
-                    tarea.fchEntrega = (DateTime)fchEntrega;
-                }
-                //Obtiene los seguidores
+                parseCodigo(jObject, tarea);
+                parseEncargado(jObject, tarea);
+                parseFchFinalizacion(jObject, tarea);
+                parseFchEntrega(jObject, tarea);
+                parseSeguidores(jObject, tarea);
             }
             return proyecto;
+        }
+
+        /**
+         * Obtiene los seguidores
+         */
+        private void parseSeguidores(JObject jObject, Tarea tarea)
+        {
+            var followers = (JArray)getObjectgFromJObject(jObject, "followers");
+            tarea.seguidores = new List<Usuario>();
+            foreach (JObject seguidor in followers)
+            {
+                Usuario usuario = parseUsuario(seguidor);
+                tarea.seguidores.Add(usuario);
+            }
+        }
+
+        /**
+         * Obtiene la fecha de entrega
+         */
+        private void parseFchEntrega(JObject jObject, Tarea tarea)
+        {
+            var fchEntrega = getObjectgFromJObject(jObject, "due_on");
+            if (fchEntrega != null)
+            {
+                tarea.fchEntrega = (DateTime)fchEntrega;
+            }
+        }
+
+        /**
+         * Obtiene la fecha de finalización
+         */
+        private void parseFchFinalizacion(JObject jObject, Tarea tarea)
+        {
+            tarea.isFinalizada = (bool)getObjectgFromJObject(jObject, "completed");
+            if (tarea.isFinalizada)
+            {
+                tarea.fchFinalizacion = (DateTime)getObjectgFromJObject(jObject, "completed_at");
+            }
+        }
+
+        /**
+         * Obtiene al encargado
+         */
+        private void parseEncargado(JObject jObject, Tarea tarea)
+        {
+            JObject assignee = (JObject) getObjectgFromJObject(jObject, "assignee");
+            tarea.encargado = parseUsuario(assignee);
+        }
+
+        private Usuario parseUsuario(JObject jObject)
+        {
+            Usuario usuario = new Usuario();
+            usuario.id = (string)getObjectgFromJObject(jObject, "gid");
+            usuario.nombre = (string)getObjectgFromJObject(jObject, "name");
+            return usuario;
+        }
+
+        /**
+         * Obtiene el id de la tarea
+         */
+        private void parseCodigo(JObject jObject, Tarea tarea)
+        {
+            tarea.codigo = (string)getObjectgFromJObject(jObject, "gid");
         }
 
         private Object getObjectgFromJObject(JObject jObject, string property)
