04db5c97e7d7a6952474a3dc2658ff292fdc4a45
emjironal <emjironal@gmail.com>
2019-05-30 13:45:29 -0600

    Guardar proyecto, sin probar

--- control/dao/DAOProyecto.cs
+++ control/dao/DAOProyecto.cs
@@ -13,24 +13,79 @@ namespace Proyecto_Diseno_Asana.control.dao
         {
             gestor.GestorBaseDatos db = new gestor.bd.PostgresBaseDatos("35.239.31.249", "postgres", "5432", "E@05face", "asana_upgradedb");
             db.conectar();
-            (new dao.DAOUsuario()).agregarUsuario(proyecto.administradorProyecto);
+            foreach(Usuario usuario in proyecto.miembros)
+            {
+                (new dao.DAOUsuario()).agregarUsuario(usuario);
+                agregarMiembroPorProyecto(usuario.id, proyecto.id);
+            }
             string query = string.Format("insert into Proyecto values ('{0}', '{1}', '{2}')", proyecto.id, proyecto.administradorProyecto.id, proyecto.nombre);
             db.executeNonQuery(query);
             foreach (Tarea seccion in proyecto.secciones)
             {
                 if(seccion.codigo != null)
+                {
                     agregarTarea(seccion, null);
+                    agregarTareaPorProyecto(proyecto.id, seccion.codigo);
+                }
                 foreach(Tarea tarea in seccion.tareas)
                 {
-                    agregarTarea(tarea, null);
-                    if (seccion.codigo != null)
-                        agregarTareaASeccion(tarea, seccion);
-                    foreach(Tarea subtarea in tarea.tareas)
-                    {
-                        agregarTarea(tarea, tarea.codigo);
-                    }
+                    agregarTarea(tarea, seccion, proyecto);
                 }
             }
+            bool result = db.executeNonQuery(query);
+            db.desconectar();
+            return result;
+        }
+
+        private void agregarTarea(Tarea tarea, Tarea seccion, Proyecto proyecto)
+        {
+            agregarTarea(tarea, null);
+            agregarTareaPorProyecto(proyecto.id, tarea.codigo);
+            if (seccion.codigo != null)
+                agregarTareaASeccion(tarea, seccion);
+            foreach (Tarea subtarea in tarea.tareas)
+            {
+                agregarTarea(tarea, tarea.codigo);
+                agregarTareaPorProyecto(proyecto.id, subtarea.codigo);
+                foreach (Usuario usuario in subtarea.seguidores)
+                {
+                    agregarSeguidorPorTarea(usuario.id, subtarea.codigo);
+                }
+            }
+            foreach (Usuario usuario in tarea.seguidores)
+            {
+                agregarSeguidorPorTarea(usuario.id, tarea.codigo);
+            }
+        }
+
+        public bool agregarMiembroPorProyecto(string idProyecto, string idUsuario)
+        {
+            gestor.GestorBaseDatos db = new gestor.bd.PostgresBaseDatos("35.239.31.249", "postgres", "5432", "E@05face", "asana_upgradedb");
+            db.conectar();
+            string query = "insert MiembroPorProyecto values ({0}, {1})";
+            query = string.Format(query, idUsuario, idProyecto);
+            bool result = db.executeNonQuery(query);
+            db.desconectar();
+            return result;
+        }
+
+        public bool agregarSeguidorPorTarea(string idUsuario, string idTarea)
+        {
+            gestor.GestorBaseDatos db = new gestor.bd.PostgresBaseDatos("35.239.31.249", "postgres", "5432", "E@05face", "asana_upgradedb");
+            db.conectar();
+            string query = "insert SeguidorPorTarea values ({0}, {1})";
+            query = string.Format(query, idUsuario, idTarea);
+            bool result = db.executeNonQuery(query);
+            db.desconectar();
+            return result;
+        }
+
+        public bool agregarTareaPorProyecto(string idProyecto, string idTarea)
+        {
+            gestor.GestorBaseDatos db = new gestor.bd.PostgresBaseDatos("35.239.31.249", "postgres", "5432", "E@05face", "asana_upgradedb");
+            db.conectar();
+            string query = "insert into TareaPorProyecto (idTarea, idProyecto) values ('{0}', '{1}')";
+            query = string.Format(query, idTarea, idProyecto);
             bool result = db.executeNonQuery(query);
             db.desconectar();
             return result;
