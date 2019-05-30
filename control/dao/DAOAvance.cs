2491e35906850b245d174479d46b494df3e58fee
emjironal <emjironal@gmail.com>
2019-05-30 14:19:27 -0600

    Agregar avance, sin evidencia

--- control/dao/DAOAvance.cs
+++ control/dao/DAOAvance.cs
@@ -11,7 +11,24 @@ namespace Proyecto_Diseno_Asana.control.dao
     {
         public Boolean crearAvance(Avance avance)
         {
-            return true;
+            gestor.GestorBaseDatos db = new gestor.bd.PostgresBaseDatos("35.239.31.249", "postgres", "5432", "E@05face", "asana_upgradedb");
+            db.conectar();
+            string query = "insert into Avance values ({0}, {1}, {2}, {3}, {4})";
+            query = string.Format(query, avance.id, avance.Fecha.ToString("yyyy-mm-dd"), avance.HorasDedicadas, avance.descripción, avance.creador.id);
+            bool result = db.executeNonQuery(query);
+            db.desconectar();
+            return result;
+        }
+
+        public bool agregarAvancePorTarea(string idTarea, string idAvance)
+        {
+            gestor.GestorBaseDatos db = new gestor.bd.PostgresBaseDatos("35.239.31.249", "postgres", "5432", "E@05face", "asana_upgradedb");
+            db.conectar();
+            string query = "insert into AvancePorTarea values ({0}, {1})";
+            query = string.Format(query, idTarea, idAvance);
+            bool result = db.executeNonQuery(query);
+            db.desconectar();
+            return result;
         }
 
         public Boolean eliminarAvance(int id)
