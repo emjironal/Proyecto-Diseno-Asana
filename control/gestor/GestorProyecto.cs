31204a0233f0120368f2efbb770df1bce78fd71b
Jouse <josuecanalesmena@gmail.com>
2019-05-30 19:10:06 -0600

    Update GestorProyecto.cs

--- control/gestor/GestorProyecto.cs
+++ control/gestor/GestorProyecto.cs
@@ -1,4 +1,5 @@
-﻿using Proyecto_Diseno_Asana.control.fabrica;
+﻿using Proyecto_Diseno_Asana.control.dao;
+using Proyecto_Diseno_Asana.control.fabrica;
 using Proyecto_Diseno_Asana.modelo;
 using System;
 using System.Collections.Generic;
@@ -14,9 +15,7 @@ namespace Proyecto_Diseno_Asana.control.gestor
         {
             Fabrica fabrica = new FabricaImportacionProyecto();
             Proyecto proyecto = (Proyecto)fabrica.fabricaProducto(json);
-            if ((new dao.DAOProyecto()).agregarProyecto(proyecto))
-                return proyecto;
-            return null;
+            return proyecto;
         }
 
         public Proyecto actualizarProyecto(String json)
@@ -27,7 +26,7 @@ namespace Proyecto_Diseno_Asana.control.gestor
 
         public Proyecto cargarProyecto(String idProyecto)
         {
-            return null;
+            DAOProyecto dao =
         }
 
         public Boolean consultar(String tipo, Object criterio)
@@ -35,9 +34,66 @@ namespace Proyecto_Diseno_Asana.control.gestor
             return true;
         }
 
-        public List<Proyecto> consultarProyectos()
+        //Mergin
+
+        public List<Usuario> mergeMiembros(Proyecto oldProyecto, Proyecto newProyecto)
         {
+            if (oldProyecto.id == newProyecto.id)
+            {
+                oldProyecto.miembros = newProyecto.miembros;
+            }
+            return oldProyecto.miembros;
+        }
+
+        public List<Tarea> MergeSecciones(Proyecto oldProyecto, Proyecto newProyecto)
+        {
+            if (oldProyecto.id == newProyecto.id)
+            {
+                foreach (Tarea seccion in newProyecto.secciones)
+                {
+                    for (int i = 0; i < oldProyecto.secciones.Count(); i++)
+                    {
+                        Tarea oldSeccion = oldProyecto.secciones.ElementAt(i);
+                        if (seccion.codigo == oldProyecto.secciones.ElementAt(i).codigo) {
+                            Tarea nueva = actualizarTarea(oldSeccion, seccion);
+                        }
+                    }
+                }
+            }
             return null;
         }
+
+        private Tarea actualizarTarea(Tarea oldSeccion, Tarea newSeccion)
+        {
+            /*
+             * isFinalizada
+             * fchFinalizacion
+             * fchEntrega
+             * notas
+             * seguidores
+             * ecanrgado
+             *
+             */
+            if (oldSeccion.codigo == newSeccion.codigo)
+            {
+                Tarea updatedTarea = newSeccion;
+                foreach (Tarea tarea in newSeccion.tareas) {
+                    for (int i = 0; i < oldSeccion.tareas.Count; i++) {
+                        if (tarea.codigo == oldSeccion.tareas.ElementAt(i).codigo) {
+                            Tarea nueva = mergeSubtarea(oldSeccion, newSeccion);
+
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        public Tarea mergeSubtarea(Tarea tarea, Tarea tarea1)
+        {
+            return tarea1;
+        }
+
+
     }
 }
