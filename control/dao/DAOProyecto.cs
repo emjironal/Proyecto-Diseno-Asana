64b9e996061947184435528076ae1cc57f4753f3
Jouse <josuecanalesmena@gmail.com>
2019-05-31 08:38:39 -0600

    lol

--- control/dao/DAOProyecto.cs
+++ control/dao/DAOProyecto.cs
@@ -26,11 +26,19 @@ namespace Proyecto_Diseno_Asana.control.dao
             return true;
         }
 
-        private Proyecto consultarProyecto(String id)
+        public Proyecto consultarProyecto(String id)
         {
-            Object[] proyAttrib = DbConnection.consultar(new Consulta().Select("*").From("Proyecto").Where(String.Format("id = {0}",id)).Get(), 3);
+            Object[] proyAttrib = DbConnection.consultar(new Consulta().Select("*").From("Proyecto").Where(String.Format("id_proyecto = '{0}'",id)).Get(), 3);
             Proyecto proyecto = new Proyecto();
-            proyecto.id = ((String[]) proyAttrib[0])[0];
+            try
+            {
+                proyecto.id = ((String[]) proyAttrib[0])[0];
+                //proyecto.administradorProyecto = 
+            }
+            catch (Exception e) {
+
+            }
+            
 
             return null;
         }
